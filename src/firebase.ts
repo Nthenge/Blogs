596707ac99d26649f0c56886ba99d306945b0e16
Nthenge <abrahamnetsec@gmail.com>
2025-06-17 14:48:06 +0300

    Add gitignore, firebase config file

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,18 +1,18 @@
-    import { initializeApp } from "firebase/app";
-    import { getAnalytics } from "firebase/analytics";
-    import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 
-    const firebaseConfig = {
-    apiKey: "AIzaSyD0vuV1tkmHbnvVDbgU9jt3meB5-nq1z3w",
-    authDomain: "mini-blog-98934.firebaseapp.com",
-    projectId: "mini-blog-98934",
-    storageBucket: "mini-blog-98934.appspot.com",
-    messagingSenderId: "330326542685",
-    appId: "1:330326542685:web:f16cd4f940b373a49100b4",
-    measurementId: "G-5TBF0DMC2X"
-    };
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+};
 
-    const app = initializeApp(firebaseConfig);
-    const analytics = getAnalytics(app);
 
-    export const db = getFirestore(app);
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const db = getFirestore(app);
